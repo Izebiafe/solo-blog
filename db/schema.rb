f5085d403b3e63cd5b6a38ea02ed72b4e73c2b8d
izebiafe <afimiafidelis95@gmail.com>
2024-01-13 17:44:23 +0100

    fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_113_162_031) do
+ActiveRecord::Schema[7.1].define(version: 20_240_113_162_154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -29,6 +29,10 @@ ActiveRecord::Schema[7.1].define(version: 20_240_113_162_031) do
   create_table 'likes', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.bigint 'post_id'
+    t.index ['post_id'], name: 'index_likes_on_post_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 
   create_table 'posts', force: :cascade do |t|
@@ -52,5 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 20_240_113_162_031) do
 
   add_foreign_key 'comments', 'posts'
   add_foreign_key 'comments', 'users'
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
