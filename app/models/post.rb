27c2cd3e1ce16835dfc81bfe7915f780344593c0
izebiafe <afimiafidelis95@gmail.com>
2024-01-13 18:44:10 +0100

    post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', counter_cache: true
   has_many :comments
   has_many :likes
 
-  def mostrecentcomments
-    comments.order(created_at: :desc).limit(5)
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_counter
+
+  def update_counter
+    author.update(posts_count: author.posts.size)
   end
-  after_create :incrementpostcounter
 
-  private
-
-  def incrementpostcounter
-    author.increment!(:posts_counter)
+  def recent_comment
+    comments.where(post_id: self[:id]).order(created_at: :desc).limit(5)
   end
 end
