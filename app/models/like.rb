07cacfe546e6fb21659a78b3761ac1e149cd206e
izebiafe <afimiafidelis95@gmail.com>
2024-01-13 18:30:00 +0100

    add likes model

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id', counter_cache: true
+
+  after_save :updatepostcounter
+
+  private
+
+  def updatepostcounter
+    post.update(likes_counter: post.likes.count)
+  end
+end
