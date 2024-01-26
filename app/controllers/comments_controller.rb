32d6a5e1b119a571e9271b15ee2dcff4b02f3c71
izebiafe <afimiafidelis95@gmail.com>
2024-01-26 09:12:57 +0100

    add routes and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+    def new
+
+    end
+    def create
+
+    end
+    def destroy
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
+  root 'users#index'
+  resources :users do
+    resources :posts do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
