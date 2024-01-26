aca6a961ff0a4d7760f5b444b5c6dfb007ef7778
izebiafe <afimiafidelis95@gmail.com>
2024-01-26 10:23:41 +0100

    add test for controllers

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,37 @@
-require 'rails_helper'
-
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET users/:user_id/posts' do
+    before :each do
+      get user_posts_path({ user_id: 1 })
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'renders the correct text' do
+      expect(response.body).to match(/this is the users post/)
+    end
+  end
+
+  describe 'GET users/:user_id/posts/:id' do
+    before :each do
+      get '/users/1/posts/1'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the correct text' do
+      expect(response.body).to match(/this is going to show the user post page/)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /' do
+    before :each do
+      get '/users'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'renders the correct text' do
+      expect(response.body).to match(/This is the users/)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    before :each do
+      get '/users/1'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the correct text' do
+      expect(response.body).to match(/This is going to be the show page/)
+    end
   end
 end
