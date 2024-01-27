c7aa7a082ed6c727f91a71719bb99cde79a6b10f
izebiafe <afimiafidelis95@gmail.com>
2024-01-27 14:40:16 +0100

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
-class PostsController < ApplicationController
-  def index; end
+ class PostsController < ApplicationController
+ def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: @user.id).includes(:comments, :user).paginate(page: params[:page], per_page: 10)
+  end
 
-  def show; end
-end
+  def show
+    logger.debug(params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+  end
+ end 
