2fc1a6754b293c093314d3582e6400dd87f4566f
izebiafe <afimiafidelis95@gmail.com>
2024-02-02 04:00:54 +0100

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,35 @@
 class PostsController < ApplicationController
+  before_action :find_user, only: [:index, :show, :new, :create]
+
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
+    @posts = @user.posts
   end
 
   def show
-    logger.debug(params)
-    @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments
   end
 
   def new
-    @user = current_user
-    @post = Post.new
-    respond_to do |format|
-      format.html { render :new }
-    end
+    @post = @user.posts.new
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = @user.posts.new(post_params)
     @post.author = current_user
+
     if @post.save
       redirect_to user_posts_path(id: current_user.id)
     else
       flash.now[:alert] = 'Cannot create a new post'
       render :new
     end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
   end
 
   def post_params
