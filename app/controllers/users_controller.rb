fa07d3fc1dacf7b7a0cbcfe6a31885dd5390c264
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-03 18:40:34 +0000

    added user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,12 @@
 # Imputcode here
+app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.all
+  end
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.recent_posts
+  end
+end
